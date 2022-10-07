e9068ea91afa5ca97570b0f1f60d1ab470b0ce0b
SemperSolus0x3d <75125391+SemperSolus0x3d@users.noreply.github.com>
2022-10-07 22:42:24 +0300

    Print current time when killing process

--- process_kill_service.py
+++ process_kill_service.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from psutil import (
     Process,
     NoSuchProcess,
@@ -11,8 +12,15 @@ class ProcessKillService:
         for process in processes:
             try:
                 process.kill()
-                print(f'Killed {process.name()} process')
+                self._report_kill(process.name())
             except AccessDenied as ex:
                 print(f'ERROR: {format_exception_only(ex)[0]}')
             except NoSuchProcess:
                 pass
+
+    def _report_kill(self, process_name):
+        datetime_str = datetime.strftime(
+            datetime.now(), '%H:%M:%S.%f'
+        )
+
+        print(f'[{datetime_str}] Killed {process_name} process')

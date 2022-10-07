6988051818f6cb216de74b9f4076fe8e24a63e60
SemperSolus0x3d <75125391+SemperSolus0x3d@users.noreply.github.com>
2022-10-07 21:03:34 +0300

    Handle exceptions when killing processes

--- process_kill_service.py
+++ process_kill_service.py
@@ -1,7 +1,18 @@
-from psutil import Process
+from psutil import (
+    Process,
+    NoSuchProcess,
+    AccessDenied
+)
+
+from traceback import format_exception_only
 
 class ProcessKillService:
     def kill_processes(self, processes: list[Process]):
         for process in processes:
-            process.kill()
-            print(f'Killed {process.name()} process')
+            try:
+                process.kill()
+                print(f'Killed {process.name()} process')
+            except AccessDenied as ex:
+                print(f'ERROR: {format_exception_only(ex)[0]}')
+            except NoSuchProcess:
+                pass
